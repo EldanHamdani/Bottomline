8d1d6090b6ba1ec18541fb887d6dcbe7c238dfbd
EHamdani <eldan.hamdani@microfocus.com>
2021-10-24 00:41:27 +0300

    dsa

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -24,6 +24,6 @@ public class HelloControllerTest {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("hello-world-$counter 0")));
+				.andExpect(content().string(equalTo("hello-world-0")));
 	}
 }
