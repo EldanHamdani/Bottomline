876c638f0b65452e428bb1b6d43f8c0eb64cb0dd
EHamdani <eldan.hamdani@microfocus.com>
2021-10-23 20:05:58 +0300

    dsa

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+	static int  counter=0
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "hello-world-$counter " + counter++;
 	}
 
 }
